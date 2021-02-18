f648eb87487194b2b94c36fea5a9e4156b44b343
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-02-18 15:26:33 +0000

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('shows a comment box', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(<App />, div);
+  expect(div.innerHTML).toContain('Comment Box');
+
+  ReactDOM.unmountComponentAtNode(div);
 });
